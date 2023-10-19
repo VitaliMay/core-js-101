0ea83e1e7c1bdefd2e7a15b1d66a0f34918d669b
VitaliMay <mvt30@mail.ru>
2023-10-19 23:09:39 +0300

    feat: number task 11

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -211,8 +211,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) { // так как 1 не является простым
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
