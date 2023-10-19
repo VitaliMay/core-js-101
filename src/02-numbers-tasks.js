029061654b9660655918e2a6f6fe99326c55d8a2
VitaliMay <mvt30@mail.ru>
2023-10-19 22:16:34 +0300

    feat: number task 07

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -133,8 +133,9 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  // throw new Error('Not implemented');
+  return value.toString().slice(-1);
 }
 
 
