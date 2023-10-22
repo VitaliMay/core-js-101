dee77b8d56adcbb51158f057919c070922b4c121
VitaliMay <mvt30@mail.ru>
2023-10-22 20:09:38 +0300

    feat: array task 29

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -687,8 +687,13 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  let result = arr;
+  indexes.map((item) => {
+    result = result[item];
+    return item;
+  });
+  return result;
 }
 
 
